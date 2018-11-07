8a92e37512c4345ae065284be3177dfb2334cde9
dsmcclain <dylan.mcclain@gmail.com>
2018-11-06 21:35:42 -0800

    add state and defredify function to HelloWorld component

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -4,13 +4,22 @@ import './HelloWorld.css';
 class HelloWorld extends Component {
   constructor(props) {
       super(props);
-      this.state = { greeting: 'Hello' };
+      this.state = { 
+        greeting: 'Hello',
+        fredCount: 0,
+      };
       this.fredify = this.fredify.bind(this);
       this.removePeople = this.removePeople.bind(this);
   }
 
   fredify() {
-      this.setState({ greeting: 'Yaba Daba Doo' });
+      const fredness = this.state.greeting;
+      if (fredness === 'Hello') {
+        this.setState({ greeting: 'Yaba Daba Doo' });
+        this.setState({ fredCount: this.state.fredCount + 1 });
+      } else {
+        this.setState({ greeting: 'Hello' });
+      };
   }
 
   removePeople() {
@@ -21,6 +30,8 @@ class HelloWorld extends Component {
     return (
       <div className="HelloWorld">
         {this.state.greeting} {this.props.name}!
+        <br />
+        You have been Fredified {this.state.fredCount} times.
         <br />
         <button className="fredify" onClick={this.fredify}>Fredify!</button>
         <button className="remove" onClick={this.removePeople}>Remove Me!</button>
