b3d906fa266a1cd1404da1f12ee7fc63e750ce55
Dylan McClain <dylan.mcclain@gmail.com>
2018-12-11 03:13:27 -0800

    add search feature to tracking widget

--- src/TrackingWidget.js
+++ src/TrackingWidget.js
@@ -4,20 +4,36 @@ class TrackingWidget extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			search: "",
             terms: {fever: 0, congestion: 2},
 		}
 	}
 
+    onChange = e => {
+        this.setState({ search: e.target.value });
+    }
+
 	render() {
 		const termList = Object.entries(this.state.terms).map(([key, value]) => {
+
+		const {search} = this.state;
+
+        if( search !== "" && key.toLowerCase().indexOf( search.toLowerCase() ) === -1 ){
+            return null
+        }
+
 			return (
 			<div className="term-list">
 				{key} : {value.toString()}
 			</div>
 			);
 		});
+
 		return (
-			<div className="TrackingWidget">{termList}</div>
+			<div className="tracking-widget">
+				<input className="tracking-searchbar" placeholder="search terms..." onChange={this.onChange} />
+				<div className="TrackingWidget">{termList}</div>
+			</div>
 		);
 	}
 }
