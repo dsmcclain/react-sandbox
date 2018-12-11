2613da66e4295fe7be4d68df5d97e443eeaad183
Dylan McClain <dylan.mcclain@gmail.com>
2018-12-11 14:15:50 -0800

    add flexbox styling to tracking widget

--- src/TrackingWidget.js
+++ src/TrackingWidget.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import './TrackingWidget.css';
 
 class TrackingWidget extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			search: "",
-            terms: {fever: 0, congestion: 2},
+            terms: {
+            	fever: 0, 
+            	congestion: 2,
+            	jaundice: 42,
+            	vomiting: 12,
+            	diarrhea: 1,
+            	tired: 12,
+			},
 		}
 	}
 
@@ -15,24 +23,26 @@ class TrackingWidget extends Component {
 
 	render() {
 		const termList = Object.entries(this.state.terms).map(([key, value]) => {
+			const {search} = this.state;
 
-		const {search} = this.state;
+	        if( search !== "" && key.toLowerCase().indexOf( search.toLowerCase() ) === -1 ){
+	            return null
+	        }
 
-        if( search !== "" && key.toLowerCase().indexOf( search.toLowerCase() ) === -1 ){
-            return null
-        }
-
-			return (
-			<div className="term-list">
-				{key} : {value.toString()}
-			</div>
-			);
+				return (
+				<div className="tracking-columns">
+					<div className="term-list">{key}</div>
+					<div className="occurrence-list">{value.toString()}</div>
+				</div>
+				);
 		});
 
 		return (
 			<div className="tracking-widget">
 				<input className="tracking-searchbar" placeholder="search terms..." onChange={this.onChange} />
-				<div className="TrackingWidget">{termList}</div>
+				<br/>
+				<button>Add Term</button>
+				{termList}
 			</div>
 		);
 	}
